bd40efbc0b779c2fcfc42257b3980e8a23c86808
Robbie Davis <robbie@therobbiedavis.com>
2026-07-22 11:18:35 -0400

    test(filesystem): add empty destination quarantine barrier

--- listenarr.infrastructure/FileSystem/FileMover.TestHooks.cs
+++ listenarr.infrastructure/FileSystem/FileMover.TestHooks.cs
@@ -4,4 +4,5 @@ public partial class FileMover
 {
     internal Func<string, Task>? AfterDirectoryCopyStagingDirectoriesCreatedForTestAsync { get; init; }
     internal Func<string, Task>? BeforeDirectoryCopyPublicationForTestAsync { get; init; }
+    internal Func<string, Task>? BeforeEmptyDestinationPlaceholderQuarantineForTestAsync { get; init; }
 }
