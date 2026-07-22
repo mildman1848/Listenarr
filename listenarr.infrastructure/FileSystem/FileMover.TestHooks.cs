622d3527e8cf9d1e2bd2bd9cda5dfbd6f0589bbf
Robbie Davis <robbie@therobbiedavis.com>
2026-07-22 14:50:38 -0400

    test(filesystem): add file shortcut race barrier

--- listenarr.infrastructure/FileSystem/FileMover.TestHooks.cs
+++ listenarr.infrastructure/FileSystem/FileMover.TestHooks.cs
@@ -3,6 +3,7 @@ namespace Listenarr.Infrastructure.FileSystem;
 public partial class FileMover
 {
     internal Action? BeforeDirectoryMoveAttemptForTest { get; init; }
+    internal Func<FileAction, string, string, Task>? BeforeFileSameContentShortcutForTestAsync { get; init; }
     internal Func<string, Task>? AfterDirectoryCopyStagingDirectoriesCreatedForTestAsync { get; init; }
     internal Func<string, Task>? BeforeDirectoryCopyPublicationForTestAsync { get; init; }
 }
