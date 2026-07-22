23d2f07eca27750974f1700aaa96f52a0d6f48c0
Robbie Davis <robbie@therobbiedavis.com>
2026-07-21 23:07:43 -0400

    testability(filesystem): expose directory publication barrier

--- listenarr.infrastructure/FileSystem/FileMover.TestHooks.cs
+++ listenarr.infrastructure/FileSystem/FileMover.TestHooks.cs
@@ -3,4 +3,5 @@ namespace Listenarr.Infrastructure.FileSystem;
 public partial class FileMover
 {
     internal Func<string, Task>? AfterDirectoryCopyStagingDirectoriesCreatedForTestAsync { get; init; }
+    internal Func<string, Task>? BeforeDirectoryCopyPublicationForTestAsync { get; init; }
 }
